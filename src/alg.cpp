c2ef987fce1ace10c046cb85d21a2ddb47e83b85
ogarkovavalentina <79150763+ogarkovavalentina@users.noreply.github.com>
2021-03-25 00:40:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = 0, right = size - 1, count = 0;
+  while (left < right) {
+  int m = (left + right) / 2;
+  if (arr[m] < value)
+  left = m + 1;
+  else
+  right = m;
+  }
+
+  if (arr[left] == value) {
+  while (arr[left] == value) {
+  count++;
+  left++;
+  }
+  }
+
+  if (count)
+  return count;
+  else
   return 0; // если ничего не найдено
 }
